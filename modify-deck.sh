6f8fd004adaf54c9d08a629e05d15da30b278fb4
Adam James-Liebau Johnson <riizade@gmail.com>
2026-01-21 15:19:51 -0500

    do not output images as part of modify-deck

--- modify-deck.sh
+++ modify-deck.sh
@@ -3,8 +3,6 @@ set -ex
 DECK_NAME=$1
 NOTE_TEXT=$2
 BINARY_LOCATION="/home/riizade/Dev/mtg-cube-helper/release/mtg-cube-helper"
-OUTPUT_IMAGES="/home/riizade/Downloads/out"
 
 $BINARY_LOCATION add-watermark --input "$DECK_NAME" --watermark Note --note-text "$NOTE_TEXT"
 $BINARY_LOCATION add-watermark --input "$DECK_NAME" --watermark Artist
-$BINARY_LOCATION prepare-images --skip-bleed-border --output-directory "$OUTPUT_IMAGES" --input-file "$DECK_NAME"
