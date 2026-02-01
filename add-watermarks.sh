0207f771d21a4e568474e16f70cb1e2830fefe61
Adam James-Liebau Johnson <riizade@gmail.com>
2026-02-01 16:14:07 -0500

    added ffcube v2 list, updated add-watermarks.sh, updated watermarks for avatar decks

--- add-watermarks.sh
+++ add-watermarks.sh
@@ -4,5 +4,9 @@ DECK_NAME=$1
 NOTE_TEXT=$2
 BINARY_LOCATION="/home/riizade/Dev/mtg-cube-helper/release/mtg-cube-helper"
 
+# remove existing watermarks
+yq ".cards[].watermarks = []" "$DECK_NAME" -i
+
+# add artist and note
 $BINARY_LOCATION add-watermark --input "$DECK_NAME" --watermark Note --note-text "$NOTE_TEXT"
 $BINARY_LOCATION add-watermark --input "$DECK_NAME" --watermark Artist
