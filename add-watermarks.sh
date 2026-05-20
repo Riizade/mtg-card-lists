9138beaec43285862bb3d59d2223033606b6cf43
Adam (riizade) Johnson <adam@riizade.com>
2026-05-20 16:15:27 -0400

    update watermarks script

--- add-watermarks.sh
+++ add-watermarks.sh
@@ -1,12 +1,13 @@
-set -ex
+#!/bin/bash
+
+set -exuo pipefail
 
 DECK_NAME=$1
 NOTE_TEXT=$2
-BINARY_LOCATION="/home/riizade/Dev/mtg-cube-helper/release/mtg-cube-helper"
 
 # remove existing watermarks
 yq ".cards[].watermarks = []" "$DECK_NAME" -i
 
 # add artist and note
-$BINARY_LOCATION add-watermark --input "$DECK_NAME" --watermark Note --note-text "$NOTE_TEXT"
-$BINARY_LOCATION add-watermark --input "$DECK_NAME" --watermark Artist
+mtg-cube-helper add-watermark --input "$DECK_NAME" --watermark Note --note-text "$NOTE_TEXT"
+mtg-cube-helper add-watermark --input "$DECK_NAME" --watermark Artist
